e39c598592a5a9b213b2d4888ed414a8062af2dc
mohammedali9810 <mohammedali9810@gmail.com>
2020-05-20 05:50:13 +0200

    Project-Files

---
+++ Project-Files/Clock-Da-Timer-V1.0/Clock + Timer/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Clock___Timer
+{
+    partial class Clock_Da_Timer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Time = new System.Windows.Forms.Label();
+            this.Tim = new System.Windows.Forms.Label();
+            this.Sec = new System.Windows.Forms.TextBox();
+            this.Min = new System.Windows.Forms.TextBox();
+            this.Hrss = new System.Windows.Forms.TextBox();
+            this.Hrs = new System.Windows.Forms.Label();
+            this.Mins = new System.Windows.Forms.Label();
+            this.Secs = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Mp3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Time.Location = new System.Drawing.Point(111, 9);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(290, 75);
+            this.Time.TabIndex = 0;
+            this.Time.Text = "00:00:00";
+            this.Time.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Tim
+            // 
+            this.Tim.AutoSize = true;
+            this.Tim.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tim.Location = new System.Drawing.Point(145, 113);
+            this.Tim.Name = "Tim";
+            this.Tim.Size = new System.Drawing.Size(220, 56);
+            this.Tim.TabIndex = 1;
+            this.Tim.Text = "00:00:00";
+            // 
+            // Sec
+            // 
+            this.Sec.Location = new System.Drawing.Point(450, 205);
+            this.Sec.Name = "Sec";
+            this.Sec.Size = new System.Drawing.Size(62, 20);
+            this.Sec.TabIndex = 2;
+            // 
+            // Min
+            // 
+            this.Min.Location = new System.Drawing.Point(251, 205);
+            this.Min.Name = "Min";
+            this.Min.Size = new System.Drawing.Size(70, 20);
+            this.Min.TabIndex = 3;
+            // 
+            // Hrss
+            // 
+            this.Hrss.Location = new System.Drawing.Point(69, 205);
+            this.Hrss.Name = "Hrss";
+            this.Hrss.Size = new System.Drawing.Size(62, 20);
+            this.Hrss.TabIndex = 4;
+            // 
+            // Hrs
+            // 
+            this.Hrs.AutoSize = true;
+            this.Hrs.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hrs.Location = new System.Drawing.Point(20, 208);
+            this.Hrs.Name = "Hrs";
+            this.Hrs.Size = new System.Drawing.Size(43, 14);
+            this.Hrs.TabIndex = 5;
+            this.Hrs.Text = "Hours:";
+            // 
+            // Mins
+            // 
+            this.Mins.AutoSize = true;
+            this.Mins.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mins.Location = new System.Drawing.Point(190, 208);
+            this.Mins.Name = "Mins";
+            this.Mins.Size = new System.Drawing.Size(55, 14);
+            this.Mins.TabIndex = 6;
+            this.Mins.Text = "Minutes:";
+            // 
+            // Secs
+            // 
+            this.Secs.AutoSize = true;
+            this.Secs.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Secs.Location = new System.Drawing.Point(386, 208);
+            this.Secs.Name = "Secs";
+            this.Secs.Size = new System.Drawing.Size(58, 14);
+            this.Secs.TabIndex = 7;
+            this.Secs.Text = "Seconds:";
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(79, 270);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(98, 23);
+            this.Start.TabIndex = 8;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop.Location = new System.Drawing.Point(251, 270);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(92, 23);
+            this.Stop.TabIndex = 9;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Mp3
+            // 
+            this.Mp3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mp3.Location = new System.Drawing.Point(414, 270);
+            this.Mp3.Name = "Mp3";
+            this.Mp3.Size = new System.Drawing.Size(89, 23);
+            this.Mp3.TabIndex = 10;
+            this.Mp3.Text = "Alarm Tone";
+            this.Mp3.UseVisualStyleBackColor = true;
+            this.Mp3.Click += new System.EventHandler(this.Mp3_Click);
+            // 
+            // Clock_Da_Timer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 305);
+            this.Controls.Add(this.Mp3);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Secs);
+            this.Controls.Add(this.Mins);
+            this.Controls.Add(this.Hrs);
+            this.Controls.Add(this.Hrss);
+            this.Controls.Add(this.Min);
+            this.Controls.Add(this.Sec);
+            this.Controls.Add(this.Tim);
+            this.Controls.Add(this.Time);
+            this.Name = "Clock_Da_Timer";
+            this.Text = "Clock-Da-Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Label Tim;
+        private System.Windows.Forms.TextBox Sec;
+        private System.Windows.Forms.TextBox Min;
+        private System.Windows.Forms.TextBox Hrss;
+        private System.Windows.Forms.Label Hrs;
+        private System.Windows.Forms.Label Mins;
+        private System.Windows.Forms.Label Secs;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Mp3;
+    }
+}
+
---
+++ Project-Files/Clock-Da-Timer-V1.0/Clock + Timer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.Diagnostics;
+
+namespace Clock___Timer
+{
+    public partial class Clock_Da_Timer : Form
+    {
+        public String FL; WindowsMediaPlayer Th = new WindowsMediaPlayer();
+        Timer T = new Timer(); Timer F = new Timer();
+        public Clock_Da_Timer()
+        {
+            InitializeComponent();
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void T_Tick(object sender, EventArgs e)
+        {
+            string clock = DateTime.Now.ToString("hh:mm:ss");
+            Time.Text = clock;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            T.Interval = 1000; F.Interval = 1000;
+            T.Tick += new EventHandler(this.T_Tick);  F.Tick += new EventHandler(this.F_Tick);
+            T.Start(); 
+        }
+        public void F_Tick(object sender, EventArgs e)
+        {
+            char [] G = Tim.Text.ToCharArray(); String h=(G[0].ToString()+G[1].ToString()),M=(G[3].ToString()+G[4].ToString()),S=(G[6].ToString()+G[7].ToString());
+            Byte HS = Byte.Parse(h),MS=Byte.Parse(M),SS=byte.Parse(S);
+
+            if (HS == 0 & MS == 0 & SS == 0)
+            {
+                F.Stop();  Th.URL = (FL); Th.controls.play();
+            }
+            if ( MS == 0 & HS == 0&HS>0)
+            {
+                SS = 59;
+                MS -= 1;
+            }
+            else if (SS < 60 & SS > 0)
+            {
+                SS -= 1;
+            }
+            else SS = 0;
+            if (MS == 0 && SS == 0&HS>0)
+            {
+                MS = 59;
+                HS -= 1;
+            }
+            else if (MS < 60 & MS > 0 & SS == 0)
+            {
+                SS = 59;
+                MS -= 1;
+            }
+            else if (MS == 0 & SS != 0)
+            {
+                MS = 0;
+            }
+              Tim.Text = string.Format("{0}:{1}:{2}", HS.ToString().PadLeft(2, '0'), MS.ToString().PadLeft(2, '0'), SS.ToString().PadLeft(2, '0'));
+            
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string hr="0", Mn="0", Sc="0";
+            if (string.IsNullOrWhiteSpace(Hrss.Text)) { hr = "00"; } else if (byte.Parse(Hrss.Text) < 10) {   hr = ("0" + Hrss.Text); }
+            else  hr = Hrss.Text.ToString();
+            if (string.IsNullOrWhiteSpace(Min.Text)) { Mn = "00"; }
+            else if (byte.Parse(Min.Text) < 10) {    Mn = ("0" + Min.Text); }
+            else Mn = Min.Text.ToString();
+            if (string.IsNullOrWhiteSpace(Sec.Text)) { Sc = "00"; }
+            else if (byte.Parse(Sec.Text) < 10) {   Sc = ("0" + Sec.Text); }
+            else Sc = Sec.Text.ToString();
+            Tim.Text = (hr + ":" + Mn + ":" + Sc);
+            F.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            F.Stop(); Th.controls.stop();
+        }
+
+        public void Mp3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Z = new OpenFileDialog();
+            //Z.DefaultExt = "";
+
+            if (Z.ShowDialog() == System.Windows.Forms.DialogResult.OK && Z.FileName.Length > 0)
+            {
+                //Process b = new Process();
+                FL = Z.FileName;
+                //b.StartInfo.FileName = Z.FileName;
+            }
+        }
+    }
+}
